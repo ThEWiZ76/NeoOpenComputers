f0de50db960ae50a6d664bdade8a8f3e570e31ae
ThEWiZ76 <roland@kleinovermeer.nl>
2026-06-19 11:25:49 +0200

    feat: port item stack array value

---
+++ src/main/java/li/cil/oc/api/prefab/ItemStackArrayValue.java
@@ -0,0 +1,129 @@
+package li.cil.oc.api.prefab;
+
+import li.cil.oc.api.machine.Arguments;
+import li.cil.oc.api.machine.Callback;
+import li.cil.oc.api.machine.Context;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.Tag;
+import net.minecraft.world.item.ItemStack;
+
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class ItemStackArrayValue extends AbstractValue {
+    private static final String ARRAY_KEY = "Array";
+    private static final String INDEX_KEY = "Index";
+    private static final HashMap<Object, Object> EMPTY_MAP = new HashMap<>();
+
+    private ItemStack[] array;
+    private int iteratorIndex;
+
+    public ItemStackArrayValue(final ItemStack[] array) {
+        if (array != null) {
+            this.array = new ItemStack[array.length];
+            for (int index = 0; index < array.length; index++) {
+                this.array[index] = array[index] != null ? array[index].copy() : null;
+            }
+        }
+        iteratorIndex = 0;
+    }
+
+    public ItemStackArrayValue() {
+        this(null);
+    }
+
+    @Override
+    public Object[] call(final Context context, final Arguments arguments) {
+        if (array == null || iteratorIndex >= array.length) {
+            return null;
+        }
+
+        final ItemStack stack = array[iteratorIndex++];
+        if (stack == null || stack.isEmpty()) {
+            return new Object[]{EMPTY_MAP};
+        }
+        return new Object[]{stack};
+    }
+
+    @Override
+    public Object apply(final Context context, final Arguments arguments) {
+        if (arguments.count() == 0 || array == null) {
+            return null;
+        }
+
+        if (arguments.isInteger(0)) {
+            final int luaIndex = arguments.checkInteger(0);
+            if (luaIndex < 1 || luaIndex > array.length) {
+                return null;
+            }
+            return array[luaIndex - 1];
+        }
+
+        if (arguments.isString(0) && "n".equals(arguments.checkString(0))) {
+            return array.length;
+        }
+
+        return null;
+    }
+
+    @Override
+    public void load(final CompoundTag nbt) {
+        if (nbt.contains(ARRAY_KEY, Tag.TAG_LIST)) {
+            final ListTag list = nbt.getList(ARRAY_KEY, Tag.TAG_COMPOUND);
+            array = new ItemStack[list.size()];
+            for (int index = 0; index < list.size(); index++) {
+                final CompoundTag stackTag = list.getCompound(index);
+                array[index] = stackTag.isEmpty()
+                        ? null
+                        : ItemStack.OPTIONAL_CODEC.parse(NbtOps.INSTANCE, stackTag).result().orElse(null);
+            }
+        } else {
+            array = null;
+        }
+        iteratorIndex = nbt.getInt(INDEX_KEY);
+    }
+
+    @Override
+    public void save(final CompoundTag nbt) {
+        if (array != null) {
+            final ListTag list = new ListTag();
+            for (final ItemStack stack : array) {
+                list.add(stack != null && !stack.isEmpty()
+                        ? ItemStack.OPTIONAL_CODEC.encodeStart(NbtOps.INSTANCE, stack).result().orElseGet(CompoundTag::new)
+                        : new CompoundTag());
+            }
+            nbt.put(ARRAY_KEY, list);
+        }
+        nbt.putInt(INDEX_KEY, iteratorIndex);
+    }
+
+    @Callback(doc = "function():nil -- Reset the iterator index so that the next call will return the first element.")
+    public Object[] reset(final Context context, final Arguments arguments) {
+        iteratorIndex = 0;
+        return null;
+    }
+
+    @Callback(doc = "function():number -- Returns the number of elements in the array.")
+    public Object[] count(final Context context, final Arguments arguments) {
+        return new Object[]{array != null ? array.length : 0};
+    }
+
+    @Callback(doc = "function():table -- Returns all stacks in the array. Memory intensive.")
+    public Object[] getAll(final Context context, final Arguments arguments) {
+        final TreeMap<Integer, Object> map = new TreeMap<>();
+        if (array != null) {
+            for (int index = 0; index < array.length; index++) {
+                final ItemStack stack = array[index];
+                map.put(index + 1, stack != null ? stack : EMPTY_MAP);
+            }
+        }
+        return new Object[]{map};
+    }
+
+    @Override
+    public String toString() {
+        return "{ItemStack Array}";
+    }
+}
---
+++ src/test/java/li/cil/oc/api/prefab/ItemStackArrayValueTest.java
@@ -0,0 +1,118 @@
+package li.cil.oc.api.prefab;
+
+import li.cil.oc.api.machine.Arguments;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+final class ItemStackArrayValueTest {
+    @Test
+    void iteratesNullSlotsAsEmptyTables() {
+        ItemStackArrayValue value = new ItemStackArrayValue(new ItemStack[]{null});
+
+        assertArrayEquals(new Object[]{Collections.emptyMap()}, value.call(null, new TestArguments()));
+        assertNull(value.call(null, new TestArguments()));
+    }
+
+    @Test
+    void supportsLuaStyleIndexAndCountAccess() {
+        ItemStackArrayValue value = new ItemStackArrayValue(new ItemStack[]{null});
+
+        assertNull(value.apply(null, new TestArguments(1)));
+        assertEquals(1, value.apply(null, new TestArguments("n")));
+    }
+
+    @Test
+    void savesAndLoadsIteratorIndexAndArrayLength() throws Exception {
+        ItemStackArrayValue value = new ItemStackArrayValue(new ItemStack[]{null});
+        value.call(null, new TestArguments());
+
+        CompoundTag tag = new CompoundTag();
+        value.save(tag);
+
+        ItemStackArrayValue loaded = new ItemStackArrayValue();
+        loaded.load(tag);
+
+        assertArrayEquals(new Object[]{1}, loaded.count(null, new TestArguments()));
+        assertNull(loaded.call(null, new TestArguments()));
+    }
+
+    @Test
+    void returnsUsefulStringName() {
+        assertEquals("{ItemStack Array}", new ItemStackArrayValue().toString());
+    }
+
+    private static final class TestArguments implements Arguments {
+        private final Object[] values;
+
+        private TestArguments(final Object... values) {
+            this.values = values;
+        }
+
+        @Override
+        public int count() {
+            return values.length;
+        }
+
+        @Override
+        public Object checkAny(final int index) {
+            return values[index];
+        }
+
+        @Override
+        public int checkInteger(final int index) {
+            return (Integer) values[index];
+        }
+
+        @Override
+        public String checkString(final int index) {
+            return (String) values[index];
+        }
+
+        @Override
+        public boolean isInteger(final int index) {
+            return values[index] instanceof Integer;
+        }
+
+        @Override
+        public boolean isString(final int index) {
+            return values[index] instanceof String;
+        }
+
+        @Override
+        public Object[] toArray() {
+            return values.clone();
+        }
+
+        @Override public boolean checkBoolean(final int index) { throw new UnsupportedOperationException(); }
+        @Override public long checkLong(final int index) { throw new UnsupportedOperationException(); }
+        @Override public double checkDouble(final int index) { throw new UnsupportedOperationException(); }
+        @Override public byte[] checkByteArray(final int index) { throw new UnsupportedOperationException(); }
+        @Override public Map checkTable(final int index) { throw new UnsupportedOperationException(); }
+        @Override public ItemStack checkItemStack(final int index) { throw new UnsupportedOperationException(); }
+        @Override public Object optAny(final int index, final Object def) { throw new UnsupportedOperationException(); }
+        @Override public boolean optBoolean(final int index, final boolean def) { throw new UnsupportedOperationException(); }
+        @Override public int optInteger(final int index, final int def) { throw new UnsupportedOperationException(); }
+        @Override public long optLong(final int index, final long def) { throw new UnsupportedOperationException(); }
+        @Override public double optDouble(final int index, final double def) { throw new UnsupportedOperationException(); }
+        @Override public String optString(final int index, final String def) { throw new UnsupportedOperationException(); }
+        @Override public byte[] optByteArray(final int index, final byte[] def) { throw new UnsupportedOperationException(); }
+        @Override public Map optTable(final int index, final Map def) { throw new UnsupportedOperationException(); }
+        @Override public ItemStack optItemStack(final int index, final ItemStack def) { throw new UnsupportedOperationException(); }
+        @Override public boolean isBoolean(final int index) { throw new UnsupportedOperationException(); }
+        @Override public boolean isLong(final int index) { throw new UnsupportedOperationException(); }
+        @Override public boolean isDouble(final int index) { throw new UnsupportedOperationException(); }
+        @Override public boolean isByteArray(final int index) { throw new UnsupportedOperationException(); }
+        @Override public boolean isTable(final int index) { throw new UnsupportedOperationException(); }
+        @Override public boolean isItemStack(final int index) { throw new UnsupportedOperationException(); }
+        @Override public Iterator<Object> iterator() { return java.util.Arrays.asList(values).iterator(); }
+    }
+}
