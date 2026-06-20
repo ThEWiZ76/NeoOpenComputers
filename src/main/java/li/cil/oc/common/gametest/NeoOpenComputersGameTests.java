3c4babf1a8a94c159f2ab446aaae5a3341cfa6c1
ThEWiZ76 <roland@kleinovermeer.nl>
2026-06-20 06:54:01 +0200

    test: verify adjacent block networking in gametest

--- src/main/java/li/cil/oc/common/gametest/NeoOpenComputersGameTests.java
+++ src/main/java/li/cil/oc/common/gametest/NeoOpenComputersGameTests.java
@@ -1,8 +1,12 @@
 package li.cil.oc.common.gametest;
 
 import li.cil.oc.NeoOpenComputers;
+import li.cil.oc.api.network.Node;
 import li.cil.oc.common.ModBlocks;
 import li.cil.oc.common.ModItems;
+import li.cil.oc.common.blockentity.ComputerCaseBlockEntity;
+import li.cil.oc.common.blockentity.DiskDriveBlockEntity;
+import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.neoforged.neoforge.gametest.GameTestHolder;
@@ -25,6 +29,26 @@ public final class NeoOpenComputersGameTests {
         ModItems.MEMORY_TIER1.get();
         ModItems.NETWORK_CARD.get();
         helper.succeed();
+    }
+
+    @GameTest(template = "empty")
+    public static void adjacentBlocksJoinSameNetwork(final GameTestHelper helper) {
+        final BlockPos computerPos = new BlockPos(1, 1, 1);
+        final BlockPos diskDrivePos = new BlockPos(2, 1, 1);
+
+        helper.setBlock(computerPos, ModBlocks.COMPUTER_CASE_TIER1.get());
+        helper.setBlock(diskDrivePos, ModBlocks.DISK_DRIVE.get());
+
+        helper.succeedWhen(() -> {
+            final ComputerCaseBlockEntity computer = helper.getBlockEntity(computerPos);
+            final DiskDriveBlockEntity diskDrive = helper.getBlockEntity(diskDrivePos);
+            final Node computerNode = computer.node();
+            final Node diskDriveNode = diskDrive.node();
+            helper.assertTrue(computerNode != null, "Computer case has no network node");
+            helper.assertTrue(diskDriveNode != null, "Disk drive has no network node");
+            helper.assertTrue(computerNode.network() != null, "Computer case node is not joined to a network");
+            helper.assertTrue(computerNode.network() == diskDriveNode.network(), "Adjacent block nodes are not in the same network");
+        });
     }
 
     private NeoOpenComputersGameTests() {
