ef4e32c46a748a5ecfd270ce65041129994380b5
ThEWiZ76 <roland@kleinovermeer.nl>
2026-06-25 16:26:27 +0200

    test(api): cover addon prefab compatibility

--- src/test/java/li/cil/oc/api/ExternalAddonCompileTest.java
+++ src/test/java/li/cil/oc/api/ExternalAddonCompileTest.java
@@ -45,16 +45,22 @@ final class ExternalAddonCompileTest {
                 import li.cil.oc.api.driver.EnvironmentProvider;
                 import li.cil.oc.api.driver.DriverItem;
                 import li.cil.oc.api.driver.InventoryProvider;
+                import li.cil.oc.api.driver.MethodWhitelist;
+                import li.cil.oc.api.driver.NamedBlock;
                 import li.cil.oc.api.machine.Arguments;
                 import li.cil.oc.api.machine.Architecture;
                 import li.cil.oc.api.machine.Callback;
                 import li.cil.oc.api.machine.Context;
                 import li.cil.oc.api.machine.ExecutionResult;
+                import li.cil.oc.api.nanomachines.Behavior;
+                import li.cil.oc.api.nanomachines.DisableReason;
                 import li.cil.oc.api.network.EnvironmentHost;
                 import li.cil.oc.api.network.ManagedEnvironment;
                 import li.cil.oc.api.network.SimpleComponent;
                 import li.cil.oc.api.network.Visibility;
+                import li.cil.oc.api.prefab.AbstractBehavior;
                 import li.cil.oc.api.prefab.AbstractManagedEnvironment;
+                import li.cil.oc.api.prefab.AbstractProvider;
                 import li.cil.oc.api.prefab.AbstractValue;
                 import net.minecraft.core.BlockPos;
                 import net.minecraft.core.Direction;
@@ -65,7 +71,9 @@ final class ExternalAddonCompileTest {
                 import net.minecraft.world.item.ItemStack;
                 import net.minecraft.world.item.Items;
                 import net.minecraft.world.level.Level;
+                import net.minecraft.world.level.block.entity.BlockEntity;
 
+                import java.util.List;
                 import java.util.Map;
 
                 public final class ExampleAddonComponent extends AbstractManagedEnvironment implements SimpleComponent {
@@ -89,13 +97,28 @@ final class ExternalAddonCompileTest {
                         Driver.add(new ExampleItemDriver());
                         Driver.add(new LegacySidedBlockDriver());
                         Driver.add(new LegacyItemDriver());
+                        Driver.add(new LegacyBlockDriver());
+                        Driver.add(new LegacyPrefabBlockDriver());
+                        Driver.add(new ExampleTileEntityDriver());
+                        Driver.add(new LegacyTileEntityDriver());
                         Driver.add(new ExampleConverter());
                         Driver.add(new ExampleEnvironmentProvider());
                         Driver.add(new ExampleInventoryProvider());
                         Machine.add(ExampleArchitecture.class);
                         FileSystem.asManagedEnvironment(FileSystem.fromMemory(1024), "addon");
                         new AbstractValue() {};
+                        new ExampleNamedBlock().preferredName();
+                        new ExampleWhitelist().whitelistedMethods();
+                        new ExampleNanomachineProvider().writeToNBT(new ExampleBehavior(playerFromHost(hostFromStack(stack))));
                         driver.worksWith(stack);
+                    }
+
+                    private static EnvironmentHost hostFromStack(final ItemStack stack) {
+                        return null;
+                    }
+
+                    private static Player playerFromHost(final EnvironmentHost host) {
+                        return null;
                     }
 
                     public static final class ExampleBlockDriver extends li.cil.oc.api.prefab.DriverSidedBlock {
@@ -114,6 +137,56 @@ final class ExternalAddonCompileTest {
                         @Override
                         public boolean worksWith(final Level world, final BlockPos pos, final Direction side) {
                             return world != null && pos != null;
+                        }
+
+                        @Override
+                        public ManagedEnvironment createEnvironment(final Level world, final BlockPos pos, final Direction side) {
+                            return new ExampleAddonComponent();
+                        }
+                    }
+
+                    @SuppressWarnings("deprecation")
+                    public static final class LegacyBlockDriver implements li.cil.oc.api.driver.Block {
+                        @Override
+                        public boolean worksWith(final Level world, final BlockPos pos) {
+                            return world != null && pos != null;
+                        }
+
+                        @Override
+                        public ManagedEnvironment createEnvironment(final Level world, final BlockPos pos) {
+                            return new ExampleAddonComponent();
+                        }
+                    }
+
+                    @SuppressWarnings("deprecation")
+                    public static final class LegacyPrefabBlockDriver extends li.cil.oc.api.prefab.DriverBlock {
+                        public LegacyPrefabBlockDriver() {
+                            super(new ItemStack(Items.STONE));
+                        }
+
+                        @Override
+                        public ManagedEnvironment createEnvironment(final Level world, final BlockPos pos, final Direction side) {
+                            return new ExampleAddonComponent();
+                        }
+                    }
+
+                    public static final class ExampleTileEntityDriver extends li.cil.oc.api.prefab.DriverSidedTileEntity {
+                        @Override
+                        public Class<?> getTileEntityClass() {
+                            return BlockEntity.class;
+                        }
+
+                        @Override
+                        public ManagedEnvironment createEnvironment(final Level world, final BlockPos pos, final Direction side) {
+                            return new ExampleAddonComponent();
+                        }
+                    }
+
+                    @SuppressWarnings("deprecation")
+                    public static final class LegacyTileEntityDriver extends li.cil.oc.api.prefab.DriverTileEntity {
+                        @Override
+                        public Class<?> getTileEntityClass() {
+                            return BlockEntity.class;
                         }
 
                         @Override
@@ -189,6 +262,56 @@ final class ExternalAddonCompileTest {
                         @Override
                         public void convert(final Object value, final Map<Object, Object> output) {
                             output.put("class", value.getClass().getName());
+                        }
+                    }
+
+                    public static final class ExampleNamedBlock implements NamedBlock {
+                        @Override
+                        public String preferredName() {
+                            return "example";
+                        }
+
+                        @Override
+                        public int priority() {
+                            return 0;
+                        }
+                    }
+
+                    public static final class ExampleWhitelist implements MethodWhitelist {
+                        @Override
+                        public String[] whitelistedMethods() {
+                            return new String[]{"getValue"};
+                        }
+                    }
+
+                    public static final class ExampleBehavior extends AbstractBehavior {
+                        public ExampleBehavior(final Player player) {
+                            super(player);
+                        }
+
+                        @Override
+                        public String getNameHint() {
+                            return "example";
+                        }
+
+                        @Override
+                        public void onDisable(final DisableReason reason) {
+                        }
+                    }
+
+                    public static final class ExampleNanomachineProvider extends AbstractProvider {
+                        public ExampleNanomachineProvider() {
+                            super("example");
+                        }
+
+                        @Override
+                        public Iterable<Behavior> createBehaviors(final Player player) {
+                            return List.of(new ExampleBehavior(player));
+                        }
+
+                        @Override
+                        protected Behavior readBehaviorFromNBT(final Player player, final CompoundTag nbt) {
+                            return new ExampleBehavior(player);
                         }
                     }
 
